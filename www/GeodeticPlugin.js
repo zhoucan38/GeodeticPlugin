56574e037d1ae8e7dc9ab88d92535f8cd872c6e3
FunkyZc <zhoucan38@126.com>
2022-06-08 17:05:54 +0800

    整理代码

--- www/GeodeticPlugin.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-const Geodetic = {
-    coolMethod = function (arg0, success, error) {
-        exec(success, error, 'Geodetic', 'coolMethod', [arg0]);
-    }
-}
-
-module.exports = Geodetic;
